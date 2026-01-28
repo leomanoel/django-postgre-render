d63e64a6d8d9885db4c20888496cc9b3e74e32e6
léo manoel lopes da silva garcia <leomanoellopesdasilvagarcia@MacBook-Air-de-leo.local>
2026-01-28 02:21:15 -0400

    fix postgres database settings for production

--- setup/settings/prod.py
+++ setup/settings/prod.py
@@ -1,9 +1,23 @@
 from .base import *
 import os
+from pathlib import Path
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ["*"]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }
+}
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 
