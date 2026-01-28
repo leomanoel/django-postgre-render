c5fc38c76dfd00a50e446a5ecb5dbe90325ebd04
léo manoel lopes da silva garcia <leomanoellopesdasilvagarcia@MacBook-Air-de-leo.local>
2026-01-28 12:28:19 -0400

    Configura MEDIA_URL em urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -9,3 +9,6 @@ urlpatterns = [
     path('', include('apps.galeria.urls')),
     path('', include('apps.usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
