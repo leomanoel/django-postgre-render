6b97591e9aef44f0d664ef38b023bc564ceb8aef
léo manoel lopes da silva garcia <leomanoellopesdasilvagarcia@MacBook-Air-de-leo.local>
2026-01-28 11:37:16 -0400

    fix whitenoise static config in production

--- setup/settings/prod.py
+++ setup/settings/prod.py
@@ -2,9 +2,20 @@ from .base import *
 import os
 from pathlib import Path
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
 
 DATABASES = {
     "default": {
@@ -17,7 +28,12 @@ DATABASES = {
     }
 }
 
+
+
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # deploy render
 
